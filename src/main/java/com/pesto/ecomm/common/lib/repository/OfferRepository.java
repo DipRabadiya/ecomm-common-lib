d0b7cf2063e828032828d81b459018569d5b7544
DipRabadiya27 <diprabadiya@gmail.com>
2024-04-05 13:08:13 +0530

    Done

--- src/main/java/com/pesto/ecomm/common/lib/repository/OfferRepository.java
+++ src/main/java/com/pesto/ecomm/common/lib/repository/OfferRepository.java
@@ -1,9 +1,12 @@
 package com.pesto.ecomm.common.lib.repository;
 
 import com.pesto.ecomm.common.lib.entity.Offer;
+import com.pesto.ecomm.common.lib.entity.Product;
+import com.pesto.ecomm.common.lib.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +17,7 @@ public interface OfferRepository extends JpaRepository<Offer, String> {
     Page<Offer> findBySeller_UserIdEqualsOrderByProduct(String sellerId, Pageable pageable);
 
     List<Offer> findByProduct_ProductId(String productId);
-
-    Offer findBySeller_UserIdAndProduct_ProductId(String sellerId, String productId);
+    @Query("select * from Offer  where seller=:seller and product=:product")
+    Offer findBySeller_UserIdAndProduct_ProductId(User seller, Product product);
 
 }
